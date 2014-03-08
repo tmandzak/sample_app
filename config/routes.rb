f33f83d1558e59363f81df65d481ae6ef7f42f60
Taras Mandzak <tmandzak@gmail.com>
2014-03-08 13:11:22 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ SampleApp::Application.routes.draw do
   # get "static_pages/about"
   # get "static_pages/contact"
 
-  
+  resources :users
   root  'static_pages#home'
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
